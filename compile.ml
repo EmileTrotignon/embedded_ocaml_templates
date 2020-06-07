c84c8d481506fcf43a64ef9ce68e4b2233019971
Emile Trotignon <emile.trotignon@laposte.net>
2020-06-07 21:40:40 +0200

    better error handling

--- compile.ml
+++ compile.ml
@@ -1,8 +1,30 @@
 open Core
 open Template
 
+type file = File of string | Directory of (string * file array)
+
 let path_readdir dirname =
   Array.map ~f:(Filename.concat dirname) (Sys.readdir dirname)
+
+let rec read_file_or_directory ?(filter = fun _ -> true) filename =
+  match Sys.is_directory filename with
+  | `Yes ->
+      Directory
+        ( filename,
+          Array.map
+            ~f:(fun file ->
+              match file with
+              | File name -> File (Filename.concat filename name)
+              | Directory (name, files) ->
+                  Directory (Filename.concat filename name, files))
+            (Array.filter
+               ~f:(fun file ->
+                 match file with File s -> filter s | Directory _ -> true)
+               (Array.map
+                  ~f:(read_file_or_directory ~filter)
+                  (Sys.readdir filename))) )
+  | `No -> File filename
+  | `Unknown -> failwith "Unknown file"
 
 let compile name header (args, elements) =
   let codes =
@@ -36,35 +58,32 @@ let compile_to_module template = compile "render" "open Core" template
 let compile_to_function name template = compile name "" template
 
 let compile_folder folder_name =
+  let directory =
+    read_file_or_directory
+      ~filter:(fun filename -> Filename.check_suffix filename ".eml")
+      folder_name
+  in
   let rec aux current_file =
-    match Sys.is_directory current_file with
-    | `Yes ->
+    match current_file with
+    | File filename -> (
+        let name = Filename.chop_extension filename in
+        let function_name = List.last_exn (Filename.parts name) in
+        match Template_builder.of_filename filename with
+        | Some template -> compile_to_function function_name template
+        | None -> failwith "Syntax error" )
+    | Directory (name, files) ->
         let module_name_bytes =
-          Bytes.of_string (List.last_exn (Filename.parts current_file))
+          Bytes.of_string (List.last_exn (Filename.parts name))
         in
         Bytes.set module_name_bytes 0
           (Char.uppercase (Bytes.get module_name_bytes 0));
         let module_name = Bytes.to_string module_name_bytes in
         sprintf "module %s = struct\n" module_name
-        ^ String.concat_array (Array.map ~f:aux (path_readdir current_file))
+        ^ String.concat_array (Array.map ~f:aux files)
         ^ "\nend\n"
-    | `No ->
-        if Filename.check_suffix current_file ".eml" then
-          let name = Filename.chop_extension current_file in
-          let function_name = List.last_exn (Filename.parts name) in
-          match Template_builder.of_filename current_file with
-          | Some template -> compile_to_function function_name template
-          | None -> ""
-        else ""
-    | `Unknown -> failwith "Unknown file"
   in
-  match Sys.is_directory folder_name with
-  | `Yes ->
-      let content =
-        String.concat_array (Array.map ~f:aux (path_readdir folder_name))
-      in
-      Out_channel.write_all (folder_name ^ ".ml") ~data:("open Core" ^ content)
-  | `No ->
+  match directory with
+  | File _ ->
       if Filename.check_suffix folder_name ".eml" then
         let name = Filename.chop_extension folder_name ^ ".ml" in
         match Template_builder.of_filename folder_name with
@@ -72,4 +91,6 @@ let compile_folder folder_name =
             Out_channel.write_all name ~data:(compile_to_module template)
         | None -> ()
       else ()
-  | `Unknown -> failwith "Unknown file"
+  | Directory (_, files) ->
+      let content = String.concat_array (Array.map ~f:aux files) in
+      Out_channel.write_all (folder_name ^ ".ml") ~data:("open Core" ^ content)
