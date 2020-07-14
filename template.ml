647cd70af6637b1c6ef60620a7a7325e99fa4c67
Emile Trotignon <emile.trotignon@gmail.com>
2020-07-14 23:10:03 +0200

    delete moved files

--- template.ml
+++
@@ -1,3 +0,0 @@
-type elt = Text of string | Code of string | Output_code of string
-
-type t = string * elt list
--- ustring.mli
+++
@@ -1,7 +0,0 @@
-type t = Uchar.t array
-
-val of_string : string -> t
-
-val print : t -> unit
-
-val to_string : t -> string
