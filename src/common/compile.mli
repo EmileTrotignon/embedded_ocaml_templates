5477f85d82c8d1f6cfdd7350ae3c0aae9cc9939d
Emile Trotignon <emile.trotignon@gmail.com>
2022-06-09 18:53:36 +0200

    documentation comment

--- src/common/compile.mli
+++ src/common/compile.mli
@@ -1,5 +1,9 @@
 
-(* Compile a single template and outputs a string containing *)
+(** [compile_to_string template] compile a single template and outputs a string
+    containing the compiled OCaml code for [template] *)
 val compile_to_string : Template.t -> string
 
+
+(** [compile_folder ~continuation_mode:cm filename] compiles a whole folder of
+    templates and write to the corresponding OCaml file. 2*)
 val compile_folder : ?continuation_mode:bool -> string -> unit
